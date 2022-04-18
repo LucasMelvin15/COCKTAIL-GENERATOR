295f37de6f5252448a542d865a5b964c0550b3a7
prime-infinity <soe.a.prime@gmail.com>
2022-04-18 21:33:56 +0100

    my commit 1

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-document.querySelector('button').addEventListener('click',getCocktail)
+document.querySelector("button").addEventListener("click", getCocktail);
 
-function getCocktail(){
-    let littyCocktail = document.querySelector('button').value
+function getCocktail() {
+  let littyCocktail = document.querySelector("button").value;
 
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php' , {
-  
-       headers:{
-           'accept':  'application/json'
-       }
-   }).then(res => res.json()) // parse response as JSON
-   .then(data => {
-       console.log(data.drinks)
-       document.querySelector('img').src= data.drinks.strDrinkThumb
-       document.querySelector('p').innerText=data.drinks.strIngredients
-       document.querySelector('h6').innerText=data.drinks.strInstructions
-   })
-     
-   
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php", {
+    headers: {
+      accept: "application/json",
+    },
+  })
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      let drink = data.drinks;
+      console.log(drink);
+      let instructions = `${
+        drink[0].strInstructions ? drink[0].strInstructions : "No instructions"
+      }`;
+      document.getElementById("ins-h").innerText = instructions;
+
+      let ingredients = `${drink[0].strIngredient1 &&
+        drink[0].strIngredient1}, ${drink[0].strIngredient2 &&
+        drink[0].strIngredient2}, ${drink[0].strIngredient3 &&
+        drink[0].strIngredient3}, ${drink[0].strIngredient4 &&
+        drink[0].strIngredient4}, ${drink[0].strIngredient5 &&
+        drink[0].strIngredient5}`;
+
+      document.querySelector("img").src = drink[0].strDrinkThumb;
+
+      document.getElementById("ing-list").innerText = ingredients;
+
+      //was trying to do something complex below, but nah
+      /*let ingrArray = ingredients.split(",");
+      for (let i = 0; i < ingrArray.length; i++) {
+        const element = ingrArray[i];
+      }*/
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
